6cf8f4a48fecf75647332f13955c41f13a215bcc
hyesech <hyesech@gmail.com>
2021-07-03 15:23:53 +0900

    move HYRO WEB path at first

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# delete existing files
+# delete existing files and navigate to HYRO WEB APP
 sudo rm -rf /home/ec2-user/hyro
 mkdir /home/ec2-user/hyro
+cd /home/ec2-user/hyro
 
 # install nodejs@14.x
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
@@ -18,12 +19,8 @@ source /home/ec2-user/.bashrc
 # install HYRO APP
 yarn install
 
-# navigate to root path
-cd ~
-sudo chown -R ec2-user:ec2-user hyro 
-
 # kill exist server
 pm2 kill
 
 # start server
-pm2 serve hyro/build 8080 --spa --name "HYRO" --env production
+pm2 serve build 8080 --spa --name "HYRO" --env production
