e73387691b36d2e906774adf118499088ccf74fe
hyesech <hyesech@gmail.com>
2021-07-03 15:09:40 +0900

    change path

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# navigate to HYRO APP DIR
+# delete existing files
 sudo rm -rf /home/ec2-user/hyro
 mkdir /home/ec2-user/hyro
-cd /home/ec2-user/hyro
 
 # install nodejs@14.x
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
@@ -19,7 +18,7 @@ source /home/ec2-user/.bashrc
 # install HYRO APP
 yarn install
 
-# change permission build directory
+# navigate to root path
 cd ~
 sudo chown -R ec2-user:ec2-user hyro 
 
@@ -27,4 +26,4 @@ sudo chown -R ec2-user:ec2-user hyro
 pm2 kill
 
 # start server
-pm2 serve build 8080 --spa --name "HYRO"
+pm2 serve hyro/build 8080 --spa --name "HYRO"
