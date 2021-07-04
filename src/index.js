f1453f1dd9d86797fd282d84c1109c3b03abf15c
hyesech <hyesech@gmail.com>
2021-07-04 23:06:07 +0900

    add BrowserRouter and main.scss

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// import Redux and Redux Saga
+
+// import Router
+import { BrowserRouter } from "react-router-dom";
+
+// import Scss
+import "./assets/scss/main.scss";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
