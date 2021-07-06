83d69ed904df66128eee1ca973085689fc37f126
hyesech <hyesech@gmail.com>
2021-07-06 19:25:37 +0900

    feat: 모바일 nav 추가

--- src/components/layout/MobileNavigation.jsx
+++ src/components/layout/MobileNavigation.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Avatar, SideSheet, Paragraph, Button, Position } from "evergreen-ui";
 
 const MobileNavigation = () => {
-    return (
-        <div>
-            <h3>Nav</h3>
-        </div>
-    )
-}
+  const [isShown, setIsShown] = useState(false);
 
-export default MobileNavigation
+  return (
+    <>
+      <SideSheet
+        isShown={isShown}
+        position={Position.BOTTOM}
+        onCloseComplete={() => setIsShown(false)}>
+        <Paragraph margin={40}>Basic Example</Paragraph>
+      </SideSheet>
+      <Avatar
+        onClick={() => setIsShown(true)}
+        src='https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg'
+        name='Alan Turing'
+        size={40}
+      />
+    </>
+  );
+};
+
+export default MobileNavigation;
