a8e9f20de5365cbe05f2295271d31bc9bfdee8b0
hyesech <hyesech@gmail.com>
2021-07-06 18:38:29 +0900

    fix: 미디어쿼리 훅 에러 픽스

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { useMediaQuery } from "react-responsive";
 // Add Layout Components
 import DesktopLayout from "./layouts/DesktopLayout";
 import MobileLayout from "./layouts/MobileLayout";
-import ContainerLayout from "./layouts/ContainerLayout";
+import SectionLayout from "./layouts/SectionLayout";
 
 function App() {
-  const isMobile = useMediaQuery({ maxDeviceWidth: 768 });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   
   return (
     <>
       { isMobile ? <MobileLayout/> : <DesktopLayout /> }
-      <ContainerLayout />
+      <SectionLayout />
     </>
   );
 }
