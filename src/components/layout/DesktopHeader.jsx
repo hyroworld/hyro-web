9dd6deb8257c5c189c339132f1e09c5bf1bb8703
hyesech <hyesech@gmail.com>
2021-07-10 19:11:37 +0900

    mobile / desktop header 마크업과 디자인 작업

--- src/components/layout/DesktopHeader.jsx
+++ src/components/layout/DesktopHeader.jsx
@@ -1,12 +1,30 @@
-/** @format */
-
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {setNavFlag} from "../../utils/reducers/globalReducer";
+import {PageHeader} from "antd";
+import {ReactComponent as Cat} from "../../assets/svgs/cat.svg";
+
 
 const DesktopHeader = () => {
+    // connection to store
+    const navFlag = useSelector((state) => state.globalReducer.NAV_FLAG);
+
+    // dispatch store
+    const dispatch = useDispatch();
+
   return (
-    <header>
-      <h1>hyro</h1>
-    </header>
+      <header className="header-container-dk">
+          <PageHeader
+              className="title-wrapper"
+              backIcon={false}
+              onBack={() => null}
+              title="HYRO"
+              subTitle="Everything begins with your stance. Remember."
+          />
+          <div className="logo-wrapper" onClick={() => dispatch(setNavFlag(!navFlag))}>
+              <Cat height={48} fill={'#0102fe'}/>
+          </div>
+      </header>
   );
 };
 
