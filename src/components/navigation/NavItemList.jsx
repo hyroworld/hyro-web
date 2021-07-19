a74f757c653333acd1da8e8750ff39bc843abcd4
hyesech <hyesech@gmail.com>
2021-07-19 19:16:01 +0900

    navigation list 렌더링

--- src/components/navigation/NavItemList.jsx
+++ src/components/navigation/NavItemList.jsx
@@ -1,14 +1,27 @@
 /** @format */
 
 import React from "react";
-import NavItem from "./NavItem";
+import { Link } from "react-router-dom";
+import {logDOM} from "@testing-library/react";
+import Enum from 'enum';
+
+const pageConfig = new Enum({
+  MAIN: ['/', 'MAIN'],
+  INFO: ['/info', 'INFORMATION'],
+  POST: ['/posts', 'POSTS'],
+  CONTACT: ['/contact', 'CONTACT'],
+  ASK: ['/ask', 'ASK']
+})
+
+const navList = pageConfig.enums.map(nav => {return <li key={nav.key}><Link to={nav.value[0]}>{nav.value[1]}</Link></li>})
 
 const NavItemList = () => {
-
   return (
-    <>
-      <NavItem />
-    </>
+    <nav>
+      <ul>
+        {navList}
+      </ul>
+    </nav>
   );
 };
 
